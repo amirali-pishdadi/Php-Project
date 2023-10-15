d9d2e6c08d3fe5e80b94b028cf809ec7f209307f
Amirali Pishdadi <ampishdadi@outlook.com>
2023-10-15 21:51:27 +0330

    Logical Operators

--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <?php 
 
-$Name = "Amirali";
+$a = true;
 
-$Family = "Pishdadi";
+$b = false;
 
-$type = 15151;
+$isLogin = false;
 
-echo "My name is \n $Name $Family";
+// Change Type = (type) => (int) & (string)
 
-var_dump($type);
+// * / - + ** % ++ 
+
+// Logical Operators => ( && and )[Both True] ( || or )[$a True Or $b True] ( Xor )[One of them True] ( !$a )[True if $a is not True.]
+
+if ($a == true and $b == false) {
+    echo "Both of them is True";
+    
+} else {
+    echo "one of them is not True";
+}
+if ($a == true xor $b == true) {
+    echo "One of them is True and the other one is false";
+
+} else {
+    echo "both of them is True of False";
+}
+if ($a == true or $b == true) {
+    echo "one or both of them is True";
+
+} else {
+    echo "none of them is True";
+}
+if (!$a) {
+    echo 'upside $a';
+
+}
 
 ?>
