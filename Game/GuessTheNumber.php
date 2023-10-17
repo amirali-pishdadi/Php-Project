81f5bb5a2a72c180392f6c51136db309f7c232c4
Amirali Pishdadi <ampishdadi@outlook.com>
2023-10-17 20:27:42 +0330

    Add some style with tailwind

--- Game/GuessTheNumber.php
+++ Game/GuessTheNumber.php
@@ -1,31 +1,43 @@
-<p>Hello , this is Guess the Number 
-    You Should guess number between 1 to 100 
-    if your guess number is true you see this message [ You Are Succsesful ]
-    but if your guess number isn't true you see this message [ try again ]
-    i hope you win the game :)
-</p>
 
-<?php 
-$user_number = 0 ;
+<?php
+$user_number = 0;
 $random_number = 0;
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $user_number = $_GET["user_number"];
-    $max = 100 ;
-    $min = 1 ;
+    $max = 100;
+    $min = 1;
     $random_number = rand($min, $max);
-
     if ($user_number == $random_number) {
-        echo "You Are Succsesful [your number : $user_number = $random_number] 😍";
+        $message = "You Are Succsesful [ your number : $user_number = $random_number ] 😍";
     } else {
-        echo "try again [your number : $user_number != $random_number] 😐";
+        $message = "try again [ your number : $user_number != $random_number ] 😐";
     }
-    
+
 }
 
 ?>
 
-<form method="GET" action="">
+<!DOCTYPE html>
+<html lang="en">
 
-<input type="number" name="user_number" placeholder="Enter your number" max="<?php echo $max; ?>" min="<?php echo $min; ?>">
-<button type="submit">Submit</button>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Guess Number</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="flex justify-center items-center py-5 font-semibold text-lg">
+        <?php echo $message ; ?>
+    </div>
+    <div class="flex py-4 justify-center items-center">
+    <form class="flex" method="GET" action="">
+        <input class="focus:outline-dashed bg-gradient-to-r from-white to-gray-500 px-2 py-1.5 w-56 rounded-lg text-black text-lg" type="number" name="user_number" placeholder="Enter your number" max="<?php echo $max; ?>"
+            min="<?php echo $min; ?>">
+        <button class="mx-2 px-6 flex-1 rounded-lg border-black outline-double py-1 font-semibold text-sm" type="submit">Submit</button>
+    </form>
+    </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,38 +1,17 @@
 <?php 
-
-$a = true;
-
-$b = false;
-
-$isLogin = false;
-
 // Change Type = (type) => (int) & (string)
 
 // * / - + ** % ++ 
 
 // Logical Operators => ( && and )[Both True] ( || or )[$a True Or $b True] ( Xor )[One of them True] ( !$a )[True if $a is not True.]
 
-if ($a == true and $b == false) {
-    echo "Both of them is True";
-    
-} else {
-    echo "one of them is not True";
-}
-if ($a == true xor $b == true) {
-    echo "One of them is True and the other one is false";
+// Array => array("sth" => "sth");
+$users = array(
+    "user1" => "Amirali",
+    "user2" => "Mahdi",
+    "user3" => "Mahan",
+);
 
-} else {
-    echo "both of them is True of False";
-}
-if ($a == true or $b == true) {
-    echo "one or both of them is True";
-
-} else {
-    echo "none of them is True";
-}
-if (!$a) {
-    echo 'upside $a';
-
-}
+echo $users["user2"];
 
 ?>
