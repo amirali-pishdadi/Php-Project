dfe8ba67604ea1dd5c99eb75d4f182c9a9f489ed
Amirali Pishdadi <ampishdadi@outlook.com>
2023-10-24 16:55:04 +0330

    Add Password Generator in Simple Project

--- Simple_Projects/OddEvenNumber.php
+++ Simple_Projects/OddEvenNumber.php
@@ -6,7 +6,7 @@ $odd = [];
 
 $even = [];
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    $user_number = isset($_GET["user_number"]) ? $_GET["user_number"] : 0 ;
+    $user_number = isset($_GET["user_number"]) ? $_GET["user_number"] : 0;
 }
 
 
@@ -39,22 +39,25 @@ while ($n <= $user_number) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="https://cdn.tailwindcss.com"></script>
-    </head>
+</head>
 
 <body>
     <div>
         <form action="" class="bg-blue-400 flex flex-col justify-center py-5" method="GET">
-        <input class="self-center focus:outline-dashed bg-gradient-to-r from-white to-gray-500 px-2 py-1.5 w-56 rounded-lg text-black text-lg" type="number" min="0" name="user_number" placeholder="Enter your number">
-        <button class="self-center mt-3 py-1.5 px-3 rounded-full border-2 border-slate-700" type="submit">Submit</button>
-    </form>
-    <div class="flex py-3">
-    <p class="flex flex-1  justify-center font-bold text-xl items-center self-center">Odd</p>
-    <p class="flex flex-1 justify-center font-bold text-xl items-center self-center">Even</p>
-    </div>
-    <div class="flex">
-        <div class="flex border-r-4 border-blue-400 flex-1 justify-center my-5">
-        <ul class="self-center ">
-                <?php for ($i = 0; $i < count($odd); $i++) {
+            <input
+                class="self-center focus:outline-dashed bg-gradient-to-r from-white to-gray-500 px-2 py-1.5 w-56 rounded-lg text-black text-lg"
+                type="number" min="0" name="user_number" placeholder="Enter your number">
+            <button class="self-center mt-3 py-1.5 px-3 rounded-full border-2 border-slate-700"
+                type="submit">Submit</button>
+        </form>
+        <div class="flex py-3">
+            <p class="flex flex-1  justify-center font-bold text-xl items-center self-center">Odd</p>
+            <p class="flex flex-1 justify-center font-bold text-xl items-center self-center">Even</p>
+        </div>
+        <div class="flex">
+            <div class="flex border-r-4 border-blue-400 flex-1 justify-center my-5">
+                <ul class="self-center ">
+                    <?php for ($i = 0; $i < count($odd); $i++) {
                         echo "<li class='font-bold px-1 py-1 text-xl'>" . $odd[$i] . "</li>";
                     }
                     ; ?>
---
+++ Simple_Projects/Password_Generator.php
@@ -0,0 +1,49 @@
+<?php
+function generatePassword($length = 8)
+{
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+|[]{}';
+    $password = "";
+
+    $charactersCount = strlen($characters);
+    for ($i = 0; $i < $length; $i++) {
+        $random = rand(0, $charactersCount - 1);
+        $password .= $characters[$random];
+
+    }
+    return $password;
+
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $length_password = isset($_GET["length"]) ? $_GET["length"] : 8;
+    $g_password = generatePassword($length_password);
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="">
+        <form class="flex bg-red-400 justify-center items-center py-4" action="" method="GET">
+            <input placeholder="Enter Your Own length ..." class="self-center focus:outline-none mx-2 px-5 py-1 rounded-full" type="number" name="length" >
+            <button class="flex bg-slate-400 px-3 py-1 rounded-full" type="submit">Send</button>
+        </form>
+        <div>
+            <h1 class="flex px-5 py-2 ">Password :</h1>
+            <p class="flex justify-center items-center text-xl font-bold"><?php echo isset($g_password) ? $g_password : "" ; ?></p>
+        </div>
+    </div>
+</body>
+
+</html>
